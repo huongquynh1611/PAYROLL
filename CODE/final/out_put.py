229c749fc08617d58900c5df34e5042b8174e0d0
huongquynh1611 <75553332+huongquynh1611@users.noreply.github.com>
2021-06-02 11:15:29 +0700

    edit code

--- CODE/final/out_put.py
+++ CODE/final/out_put.py
@@ -1,7 +1,8 @@
 from cal_payment import cal_payment
-import pandas as pd
+from datetime import date
 import time
 
 _time = time.time()
-output = cal_payment().sort_values(['ContactID','Start Period','Start Date']).reset_index(drop=True).to_excel("report_0106.xlsx")
+today = date.today().strftime("%d%m%y")
+output = cal_payment().sort_values(['ContactID','Start Period','Start Date']).reset_index(drop=True).to_excel("report_" + str(today) +".xlsx")
 print("Time elapsed: ", time.time() -  _time)
